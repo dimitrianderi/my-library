758ba7c07f6047006d65ec713e5da8d7ba18e0d3
Dmitry Kharitonov <dimitrianderi@gmail.com>
2023-10-06 22:44:17 +0500

    feat: add validation for auth

---
+++ src/use/useLoginForm.js
@@ -0,0 +1,54 @@
+import { useField, useForm } from 'vee-validate'
+import { computed, ref, watch } from 'vue'
+import * as yup from 'yup'
+
+export default function useLoginForm() {
+    const { handleSubmit, isSubmitting, submitCount } = useForm()
+    const MIN_PASS = 6
+    const isErrorEmail = ref(false)
+    const isErrorPass = ref(false)
+
+    const { value: email, errorMessage: errorEmail } = useField(
+        'email',
+        yup
+            .string()
+            .trim()
+            .email('aдрес почты введён некорректно')
+            .required('заполните это поле')
+    )
+    const { value: pass, errorMessage: errorPass } = useField(
+        'pass',
+        yup
+            .string()
+            .trim()
+            .required('заполните это поле')
+            .min(MIN_PASS, `в пароле должно быть минимум ${MIN_PASS} символов`)
+    )
+
+    const onSubmit = handleSubmit((values) => {
+        console.log(values)
+    })
+
+    watch(submitCount, (newValue) => {
+        if (newValue > 0) {
+            isErrorEmail.value = true
+            isErrorPass.value = true
+        }
+    })
+
+    watch([email, pass], ([newEmail, newPass], [oldEmail, oldPass]) => {
+        submitCount.value = 0;
+
+        isErrorEmail.value = newEmail !== oldEmail ? false : isErrorEmail.value;
+        isErrorPass.value = newPass !== oldPass ? false : isErrorPass.value;
+    });
+
+    return {
+        email,
+        errorEmail: computed(() => isErrorEmail.value ? errorEmail.value : ''),
+        pass,
+        errorPass: computed(() => isErrorPass.value ? errorPass.value : ''),
+        isSubmitting,
+        onSubmit,
+    }
+}
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,59 +1,56 @@
 <template>
-  <form class="modal">
+  <form class="modal" @submit.prevent="onSubmit">
     <h2 class="title">Авторизация</h2>
     <div class="form-control">
       <label class="label" for="email">Почта:</label>
-      <input class="input" type="email" id="email" v-model="email" />
+      <input
+        :class="['input', { error: errorEmail }]"
+        type="email"
+        id="email"
+        v-model="email"
+        autocomplete="on"
+      />
     </div>
-    <small class="error"></small>
+    <small class="errorText">{{ errorEmail }}</small>
     <div class="form-control">
       <label class="label" for="pass">Пароль:</label>
-      <input class="input" type="password" id="pass" v-model="email" />
+      <input
+        :class="['input', { error: errorPass }]"
+        type="password"
+        id="pass"
+        v-model="pass"
+        autocomplete="off"
+      />
     </div>
-    <small class="error"></small>
-    <button class="btn" id="isInBasket">Войти</button>
+    <small class="errorText">{{ errorPass }}</small>
+    <button type="submit" class="btn" id="isInBasket" :disabled="isSubmitting">Войти</button>
     <h2 class="title"></h2>
   </form>
 </template>
 
 <script>
-export default {}
+import useLoginForm from '@/use/useLoginForm'
+
+export default {
+  setup() {
+    return {
+      ...useLoginForm(),
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
 @import '@/styles/index.scss';
-.modal {
-  @include modal;
 
-  .title {
-    @include title;
+.form-control {
+  &:first-of-type {
+    margin-top: 10px;
   }
+}
 
-  .form-control {
-    @include form-control;
-
-    &:first-of-type {
-      margin-top: 10px;
-    }
-
-    .label {
-      @include label;
-    }
-
-    .input {
-      @include input;
-    }
-  }
-
-  .error {
-    @include error;
-    margin-right: 10px;
-  }
-
-  .btn {
-    margin: 10px 0;
-    align-self: center;
-    @include btn;
-  }
+.btn {
+  margin: 10px 0;
+  align-self: center;
 }
 </style>
